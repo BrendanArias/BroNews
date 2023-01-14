5e50f78a3532e55c7ae21515ab2b19e94f7af0b4
BrendanArias <107963102+BrendanArias@users.noreply.github.com>
2023-01-13 16:39:22 -0800

    Update News.py

--- News.py
+++ News.py
@@ -1,28 +1,3 @@
 """
 # Bro News: The most college way to read your news
 """
-
-/*import requests
-from bs4 import BeautifulSoup
- 
-def broify_text(url):
-    # Get the text from the website
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    text = soup.get_text()
-
-    # Replace all pronouns with "bro"
-    text = text.replace(" he ", " bro ")
-    text = text.replace(" she ", " bro ")
-    text = text.replace(" him ", " bro ")
-    text = text.replace(" her ", " bro ")
-    text = text.replace(" his ", " bro's ")
-    text = text.replace(" hers ", " bro's ")
-
-    # Output the modified text
-    print(text)
-
-# Example usage:
-url = input("Enter a URL: ")
-broify_text(url)
-*/
