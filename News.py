e1eaf562e167cb4801fb621ec7860f3abeb7b22b
BrendanArias <107963102+BrendanArias@users.noreply.github.com>
2023-01-13 16:33:24 -0800

    Update News.py

--- News.py
+++ News.py
@@ -1,55 +1,23 @@
 import requests
-import openai
-import streamlit as st
+from bs4 import BeautifulSoup
 
-# UI Text
+def broify_text(url):
+    # Get the text from the website
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    text = soup.get_text()
 
-"""
-# Bro News: The best way to read your news
-"""
-st.markdown("_Welcome to QuickNews, the fastest way to summarize articles and news reports!_")
+    # Replace all pronouns with "bro"
+    text = text.replace(" he ", " bro ")
+    text = text.replace(" she ", " bro ")
+    text = text.replace(" him ", " bro ")
+    text = text.replace(" her ", " bro ")
+    text = text.replace(" his ", " bro's ")
+    text = text.replace(" hers ", " bro's ")
 
-# Set the URL that you want to make a request to.
-article_url = st.text_input("_Enter an article's URL:_")
-st.markdown("_Created by Brendan Arias_")
+    # Output the modified text
+    print(text)
 
-# Feeds API Key From StreamLit "Secrets"
-openai.api_key = st.secrets["API_KEY"]
-
-def summarize():
-    # State starting text variable with GPT prompt.
-    text = "Summarize this information in 400 characters: "
-    
-    # Make the request and store the response.
-    response = requests.get(article_url)
-    
-    # Parse the HTML content of the website.
-    soup = BeautifulSoup(response.content, "html.parser")
-    
-    # Extract the text from the website and print it.
-    for p in soup.find_all("p"):
-      text += p.text
-
-    # Calculate estimated token count for request.
-    estimated_tokens = len(text) / 5.1
-    
-    if estimated_tokens <= 3500:
-        response = openai.Completion.create(
-            engine="text-davinci-003",
-            prompt=text,
-            temperature=1,
-            max_tokens=500
-        )
-        return response["choices"][0]["text"]
-    else:
-        return "Article too long... try another article!"
-
-
-if article_url:
-    # Text generation spinner
-    with st.spinner("Please wait while your summary is being generated..."):
-        # Generate the summarization text
-        summary = summarize()
-
-    # Feed the summarization text to the app
-    st.write(summary)
+# Example usage:
+url = input("Enter a URL: ")
+broify_text(url)
