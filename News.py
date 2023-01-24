463168aa0b35601115837336101409b115654a8d
BrendanArias <107963102+BrendanArias@users.noreply.github.com>
2023-01-23 19:02:50 -0800

    Update News.py

--- News.py
+++ News.py
@@ -1,3 +1,13 @@
 import requests
 from bs4 import BeautifulSoup4
-import streamlit as st
+
+url = "https://www.foxnews.com/politics/democratic-house-leader-hakeem-jeffries-demands-mccarthy-reappoint-schiff-swalwell-intelligence-committee"
+response = requests.get(url)
+soup = BeautifulSoup(response.text, 'html.parser')
+
+# Find the main article element
+article = soup.find('article')
+
+# Extract the text of the article
+article_text = article.get_text()
+print(article_text)
