29b065ad3b0e6080c6bfc5f8bcc76d4d7a57829e
BrendanArias <107963102+BrendanArias@users.noreply.github.com>
2023-01-24 15:52:35 -0800

    Update News.py

--- News.py
+++ News.py
@@ -22,7 +22,7 @@ def broify():
     soup = BeautifulSoup(response.content, "html.parser")
     
     # Extract the text from the website and print it.
-    text = soup.find_all("p")
+    text = soup.find_all("body, 'article')
 
 if article_url:
     # Text generation spinner
