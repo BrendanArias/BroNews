baac4cf621a1cd36ba9535768f9c76b4762ec9fe
BrendanArias <107963102+BrendanArias@users.noreply.github.com>
2023-01-24 15:45:20 -0800

    Update News.py

--- News.py
+++ News.py
@@ -6,19 +6,14 @@ import streamlit as st
 # UI Text
 
 """
-# QuickNews: _News & Article Summarizer_
+# BroNews: _News for Bros, by Bros_
 """
-st.markdown("Welcome to QuickNews, the fastest way to summarize articles and news reports!")
+
 # Set the URL that you want to make a request to.
 article_url = st.text_input("_Enter an article's URL:_")
-st.markdown("_Created by [Adam Gilani](https://twitter.com/adamgilani)_")
 
-# Feeds API Key From StreamLit "Secrets"
-openai.api_key = st.secrets["API_KEY"]
 
-def summarize():
-    # State starting text variable with GPT prompt.
-    text = "Summarize this information in 400 characters: "
+def broify():
     
     # Make the request and store the response.
     response = requests.get(article_url)
@@ -30,26 +25,11 @@ def summarize():
     for p in soup.find_all("p"):
       text += p.text
 
-    # Calculate estimated token count for request.
-    estimated_tokens = len(text) / 5.1
-    
-    if estimated_tokens <= 3500:
-        response = openai.Completion.create(
-            engine="text-davinci-003",
-            prompt=text,
-            temperature=1,
-            max_tokens=500
-        )
-        return response["choices"][0]["text"]
-    else:
-        return "Article too long... try another article!"
-
-
 if article_url:
     # Text generation spinner
     with st.spinner("Please wait while your summary is being generated..."):
         # Generate the summarization text
-        summary = summarize()
+        bronews = broify()
 
     # Feed the summarization text to the app
-    st.write(summary)
+    st.write(bronews)
