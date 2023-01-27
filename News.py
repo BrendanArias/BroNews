f655614b1ce3165042814c2b50dada744f9b1605
BrendanArias <107963102+BrendanArias@users.noreply.github.com>
2023-01-26 21:07:52 -0800

    Update News.py

--- News.py
+++ News.py
@@ -83,7 +83,7 @@ if article_url:
             new_name_list += name + "?" + "/"
         new_name_list = new_name_list.split("/")
 
-        newer_text = ' '.join(new_text)
+        newer_text = ""
         for target in new_text:
             if target:
                 punctuation = target[-1]
